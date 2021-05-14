c5f41d7d190a75a20d7a1de64a8402653d56bacf
Christoph Froehlich <christoph.froehlich@spaceteam.at>
2021-05-14 12:11:52 +0200

    create basic functions for repeated simulation calls

--- src/ortools/dispersion_analysis.py
+++ src/ortools/dispersion_analysis.py
@@ -1,7 +1,9 @@
 import ortools.utility as utility
 
 import orhelper
-import numpy
+import numpy as np
+import math
+from random import gauss
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 import click
@@ -25,6 +27,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               help="Name of the file the output is saved to.")
 @click.option("--show", "-s", is_flag=True, default=False,
               help="Show the output on screen.")
+
 def cli(directory, filename, config, output, show):
     """Do a dispersion analysis of an OpenRocket simulation.
 
@@ -50,6 +53,113 @@ def cli(directory, filename, config, output, show):
     config.read(config_file_path)
     print("config sections: {}".format(config.sections()))
 
+    # add landing point class, and add simulations
+    points = LandingPoints()
+    
+    number_iterations = int(config["General"]["NumberOfIterations"])
+    print('Start ', number_iterations, ' simulation(s).')    
+    points.set_path(ork_file_path)
+    points.set_launch_rail_deviation(float(config["LaunchRail"]["Azimuth"]), float(config["LaunchRail"]["Elevation"]))
+    points.add_simulations(number_iterations)
+
+    # statistics
+    if output_is_shown:
+        points.print_stats()
+    
+class LandingPoints(list):
+    "A list of landing points with ability to run simulations and populate itself"
+
+    def __init__(self):
+        self.ranges = []
+        self.bearings = []
+
+    def set_path(self, path):
+        self.ork_file_path = path
+
+    def set_launch_rail_deviation(self, dev_azimuth, dev_elevation):
+        self.dev_azimuth = dev_azimuth;
+        self.dev_elevation = dev_elevation;
+        print('Use deviation for azimut ', dev_azimuth, ' and elevation ', dev_elevation)
+
+    def add_simulations(self, num):
+        with orhelper.OpenRocketInstance() as instance:
+
+            # Load the document and get simulation
+            orh = orhelper.Helper(instance)
+            doc = orh.load_doc(self.ork_file_path)
+            sim = doc.getSimulation(0)
+            print('Loaded Simulation: "', sim.getName(), '"')
+
+            # Randomize various parameters
+            opts = sim.getOptions()
+            rocket = opts.getRocket()
+            
+            elevation = math.degrees(opts.getLaunchRodAngle())
+            azimuth = math.degrees(opts.getLaunchRodDirection())
+            print('Initial launch rail elevation ', elevation, ' and azimuth ', azimuth)
+
+            # Run num simulations and add to self
+            for p in range(num):
+                print('Running simulation ', p+1, ' of ', num)
+
+                opts.setLaunchRodAngle(math.radians(gauss(elevation, self.dev_elevation))) 
+                opts.setLaunchRodDirection(math.radians(gauss(azimuth, self.dev_azimuth))) 
+
+                windlistener = WindListen()  # simulation listener
+                lp = LandingPoint(self.ranges, self.bearings)
+                orh.run_simulation(sim, listeners=(windlistener, lp))
+                self.append(lp)
+
+    def print_stats(self):
+        print(
+            'Rocket landing zone %3.2f m +- %3.2f m bearing %3.2f deg +- %3.4f deg from launch site. Based on %i simulations.' % \
+            (np.mean(self.ranges), np.std(self.ranges), np.degrees(np.mean(self.bearings)),
+             np.degrees(np.std(self.bearings)), len(self)))
+        
+class LandingPoint(orhelper.AbstractSimulationListener):
+    def __init__(self, ranges, bearings):
+        self.ranges = ranges
+        self.bearings = bearings
+
+    def endSimulation(self, status, simulation_exception):
+        worldpos = status.getRocketWorldPosition()
+        conditions = status.getSimulationConditions()
+        launchpos = conditions.getLaunchSite()
+        geodetic_computation = conditions.getGeodeticComputation()
+
+        if geodetic_computation != geodetic_computation.FLAT:
+            raise Exception("GeodeticComputationStrategy type not supported")
+
+        self.ranges.append(range_flat(launchpos, worldpos))
+        self.bearings.append(bearing_flat(launchpos, worldpos))
+
+        
+class WindListen(orhelper.AbstractSimulationListener):
+    # set the wind speed as a function of altitude
+    def postWindModel(self, status, wind):
+        #print(wind)
+        #position = status.getRocketPosition()
+        #print('Altitude ', position.z)
+        wind = wind.setX(1)
+        wind = wind.setY(1)
+        #print(wind)
+        return wind
+
+
+METERS_PER_DEGREE_LATITUDE = 111325
+METERS_PER_DEGREE_LONGITUDE_EQUATOR = 111050
+
+
+def range_flat(start, end):
+    dy = (end.getLatitudeDeg() - start.getLatitudeDeg()) * METERS_PER_DEGREE_LATITUDE
+    dx = (end.getLongitudeDeg() - start.getLongitudeDeg()) * METERS_PER_DEGREE_LONGITUDE_EQUATOR
+    return math.sqrt(dy * dy + dx * dx)
+
+
+def bearing_flat(start, end):
+    dy = (end.getLatitudeDeg() - start.getLatitudeDeg()) * METERS_PER_DEGREE_LATITUDE
+    dx = (end.getLongitudeDeg() - start.getLongitudeDeg()) * METERS_PER_DEGREE_LONGITUDE_EQUATOR
+    return math.pi / 2 - math.atan(dy / dx)
 
 if __name__ == "__main__":
     cli()
